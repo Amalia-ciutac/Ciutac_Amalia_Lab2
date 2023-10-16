b896d5e97c3137a45637fa364b3969b3e067c7ff
Amalia Ciutac <amalia.ciutac@stud.ubbcluj.ro>
2023-10-16 18:49:22 +0300

    Add project files.

---
+++ Ciutac_Amalia_Lab2/Data/Ciutac_Amalia_Lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ciutac_Amalia_Lab2.Models;
+
+namespace Ciutac_Amalia_Lab2.Data
+{
+    public class Ciutac_Amalia_Lab2Context : DbContext
+    {
+        public Ciutac_Amalia_Lab2Context (DbContextOptions<Ciutac_Amalia_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ciutac_Amalia_Lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
